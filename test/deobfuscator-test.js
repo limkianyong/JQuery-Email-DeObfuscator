4a08b0e8118de8aa718bc20a6b28f2fcdddfcc53
Kian Yong Lim <limkianyong@gmail.com>
2011-05-20 20:55:23 +0100

    Added test for custom at and dot settings

--- test/deobfuscator-test.js
+++ test/deobfuscator-test.js
@@ -1,14 +1,19 @@
 (function() {
+
+	function arrangeAnchorLinkForEmailWith(obfuscatedEmail, emailLinkId) {
+		$('#qunit-fixture').append(
+			$('<a>' + obfuscatedEmail + '</a>').attr('id', emailLinkId)
+						.attr('href', obfuscatedEmail)
+		);
+	}
+
 	module("Show the correct symbols to be replaced");
 	
 	test("Replace '[at]' with '@' and '[dot]' with '.' in the href and text", function(){
 		// arrange
 		var obsfucatedEmail = "mailto:myemail [at] mydomain [dot] com [dot] my";
 		var emailLinkId = 'emailLink';
-		$('#qunit-fixture').append(
-			$('<a>' + obsfucatedEmail + '</a>').attr('id', emailLinkId)
-						.attr('href', obsfucatedEmail)
-		);
+		arrangeAnchorLinkForEmailWith(obsfucatedEmail, emailLinkId);
 		
 		var expectedEmail = "mailto:myemail@mydomain.com.my";
 		
@@ -16,7 +21,30 @@
 		$('#' + emailLinkId).deobsfucate();
 		
 		// assert
+		assertHrefAndTextEquals(emailLinkId, expectedEmail);
+	});
+	
+	test("Given custom config settings for at, deobfuscate should replace custom at with '@'", function() {
+		// arrange 
+		var obsfucatedEmail = "mailto:customEmail customAt mydomain customDot com customDot my";
+		var emailLinkId = 'emailLink2';
+		arrangeAnchorLinkForEmailWith(obsfucatedEmail, emailLinkId);
+		
+		var expectedEmail = "mailto:customEmail@mydomain.com.my";
+		
+		// act
+		$('#' + emailLinkId).deobsfucate({
+			at: 'customAt',
+			dot: 'customDot'
+		});
+		
+		// assert
+		assertHrefAndTextEquals(emailLinkId, expectedEmail);
+	});
+	
+	function assertHrefAndTextEquals(emailLinkId, expectedEmail) {	
 		equal($('#' + emailLinkId).attr('href'), expectedEmail);
 		equal($('#' + emailLinkId).text(), expectedEmail);
-	});
+	};
+	
 })();
